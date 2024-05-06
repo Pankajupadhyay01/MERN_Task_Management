b60f783dd309199262a0338b74a5a056e8df9e60
pankaj upadhyay <pankajup.git01@gmail.com>
2024-05-06 23:05:14 +0530

    helmet added

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,17 @@ const bodyParser = require("body-parser")
 const app = express()
 const cors = require("cors")
 
+const helmet = require("helmet")
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
+
+app.use(helmet())
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
